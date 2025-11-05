0f89c7470d743d7ee1e14a8c7350bce30f1518aa
aselya-coder <aselyamagangjogja@gmail.com>
2025-11-05 12:09:19 +0700

    edit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,10 +60,10 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
-    // Spatie Permission middleware aliases
-    'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
-    'roles' => \Spatie\Permission\Middleware\RoleMiddleware::class, // Added an alias
-    'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
-    'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        // Spatie Permission middleware aliases
+        'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        'roles' => \Spatie\Permission\Middleware\RoleMiddleware::class, // Added an alias
+        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
     ];
 }
